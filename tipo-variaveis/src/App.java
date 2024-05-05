8d202fb96c797157a0a45b5458adecbbc160603a
Renato <nato_rodrigues@yahoo.com.br>
2024-05-05 16:55:51 -0300

    tipos-variaveis-234

---
+++ tipo-variaveis/src/App.java
@@ -0,0 +1,33 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       // tipos primitivos
+       // estudar a classe String que representa texto na aplicação
+
+       String meuNome = "Renato Rodrigues";
+       
+       
+       
+        // double salarioMinimo = 2500;
+        //byte idade = 123;
+       // short ano = 2024;
+       // int cep = 14845190; // se começar com zero, talvez tenha que ser outro tipo
+        //long cpf = 78328041124L; // se começar com zero, talvez tenha que ser outro tipo
+        // float pi = 3.14F;
+        // double salario = 1275.33;
+        // tipos de variaveis
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        int numero = 5;
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        
+
+    
+    }
+}
