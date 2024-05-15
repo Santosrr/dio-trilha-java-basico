cb685f8537a301ab96358aab65145320f42e38c3
Renato <nato_rodrigues@yahoo.com.br>
2024-05-15 18:43:34 -0300

    AboutMe

--- MinhaClasse/AboutMe.java
+++ MinhaClasse/AboutMe.java
@@ -1,15 +1,18 @@
 public class AboutMe {
 
     public static void main(String[] args) {
+        if (args.length < 4) {
+            System.out.println("Por favor, forneça nome, sobrenome, idade e altura como argumentos.");
+            return;
+        }
+        
         String nome = args[0];
         String sobrenome = args[1];
         int idade = Integer.valueOf(args[2]);
         double altura = Double.valueOf(args[3]);
 
-
-        System.out.println("Olá, me chamo" + nome + " " + sobrenome);
-        System.out.println("Tenho" + idade + "anos");
-        System.out.println("Minha altura é" + altura + "cm") ;
-
+        System.out.println("Olá, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos");
+        System.out.println("Minha altura é " + altura + " cm");
     }
 }
